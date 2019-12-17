6ac12324438bde257c2461bce0ffe3dffdac884b
Hameha Studio <49145889+HamehaStudio@users.noreply.github.com>
2019-12-17 15:35:33 +0700

    init

---
+++ wpe-plugin-stater/includes/index.php
@@ -0,0 +1,14 @@
+<?php
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// include "helper.class.php";
+// include "posttypes.class.php";
+// include "options.class.php";
+// include "filter.class.php";
+
+if(class_exists('WooCommerce')) {
+	include "woo.class.php";
+}
